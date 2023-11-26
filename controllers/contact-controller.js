632121130a5e92851800e7d3318f44edb15273c1
Volodymyr <buvoro@gmail.com>
2023-11-26 18:21:15 +0200

    done

--- controllers/contact-controller.js
+++ controllers/contact-controller.js
@@ -1,47 +1,50 @@
-// const HttpError = require("../helpers/HttpError");
+const HttpError = require("../helpers/HttpError");
 const contactDecorator = require("../decorators/contact-decorator");
-const Contact = require("../models/Contact");
+const { Contact } = require("../models/Contact");
 
 const getAll = async (req, res) => {
   const allContacts = await Contact.find();
   res.json(allContacts);
 };
 
-// const getById = async (req, res) => {
-//   const contactById = await contactsDb.getContactById(req.params.contactId);
-//   if (!contactById) {
-//     throw HttpError(404, "Not found");
-//   }
-//   res.json(contactById);
-// };
+const getById = async (req, res) => {
+  const contactById = await Contact.findById(
+    req.params.contactId,
+    "-updatedAt -createdAt -_id"
+  );
+  if (!contactById) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(contactById);
+};
 
-// const add = async (req, res) => {
-//   res.status(201).json(await contactsDb.addContact(req.body));
-// };
+const add = async (req, res) => {
+  res.status(201).json(await Contact.create(req.body));
+};
 
-// const remove = async (req, res) => {
-//   const isDeleted = await contactsDb.removeContact(req.params.contactId);
-//   if (!isDeleted) {
-//     throw HttpError(404, "Not found");
-//   }
-//   res.json({ message: "contact deleted" });
-// };
+const remove = async (req, res) => {
+  const isDeleted = await Contact.findByIdAndDelete(req.params.contactId);
+  if (!isDeleted) {
+    throw HttpError(404, "Not found");
+  }
+  res.json({ message: "contact deleted" });
+};
 
-// const update = async (req, res) => {
-//   const contact = await contactsDb.updateContact(
-//     req.params.contactId,
-//     req.body
-//   );
-//   if (!contact) {
-//     throw HttpError(404, "Not found");
-//   }
-//   res.json(contact);
-// };
+const update = async (req, res) => {
+  const contact = await Contact.findByIdAndUpdate(
+    req.params.contactId,
+    req.body
+  );
+  if (!contact) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(contact);
+};
 
 module.exports = {
   getAll: contactDecorator(getAll),
-  // getById: contactDecorator(getById),
-  // add: contactDecorator(add),
-  // remove: contactDecorator(remove),
-  // update: contactDecorator(update),
+  getById: contactDecorator(getById),
+  add: contactDecorator(add),
+  remove: contactDecorator(remove),
+  update: contactDecorator(update),
 };
--- models/Contact.js
+++ models/Contact.js
@@ -1,12 +1,63 @@
+const Joi = require("joi");
 const { Schema, model } = require("mongoose");
+const { handleSaveError, preUpdate } = require("./hooks");
 
-const contactSchema = Schema({
-  name: String,
-  email: String,
-  phone: String,
-  favorite: Boolean,
-});
+const contactSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+contactSchema.post("save", handleSaveError);
+
+contactSchema.pre("findOneAndUpdate", preUpdate);
+contactSchema.post("findOneAndUpdate", handleSaveError);
 
 const Contact = model("contact", contactSchema);
 
-module.exports = Contact;
+const newContactSchema = Joi.object({
+  name: Joi.string()
+    .required()
+    .messages({ "any.required": "missing required name" }),
+  email: Joi.string()
+    .required()
+    .messages({ "any.required": "missing required email" }),
+  phone: Joi.string()
+    .required()
+    .messages({ "any.required": "missing required phone" }),
+  favorite: Joi.boolean(),
+});
+
+const updateContactSchema = Joi.object({
+  name: Joi.string(),
+  email: Joi.string(),
+  phone: Joi.string(),
+  favorite: Joi.boolean(),
+});
+
+const updateContactFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
+
+module.exports = {
+  Contact,
+  newContact: newContactSchema,
+  updateContact: updateContactSchema,
+  updateFavorite: updateContactFavoriteSchema,
+};
